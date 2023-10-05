bb27f19783f50babc264ff406b79afebfd934ba5
ldroll <104579967+ldroll@users.noreply.github.com>
2023-10-05 22:16:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,4 +10,11 @@ fi
 python3 -m pip install pip --upgrade
 python3 -m pip install -r requirements.txt
 
+# Download .tflite model (private folder because size > 25 GB).
+FILE=${DATA_DIR}/saved-model_metal-inspection.tflite
+if [ ! -f "$FILE" ]; then
+  curl \
+    -L 'https://drive.google.com/uc?export=download&id=1uFwNPj7hZ9tE00fUzr5Fiup_U4_2xRe2' \
+    -o ${FILE}
+fi
 echo -e "Downloaded files are in ${DATA_DIR}"
